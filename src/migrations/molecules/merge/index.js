e598fef8e241031db6d15ffa15b9d4252097d312
Wilhelm Behncke <behncke@sitegeist.de>
2016-02-17 19:05:53 +0100

    TASK: Implement merge operation for vanilla objects

---
+++ src/migrations/molecules/merge/index.js
@@ -0,0 +1,26 @@
+import createPolymorphFunction from '../../../util/createPolymorphFunction/index.js';
+import resolveObjectPath from '../../../util/resolveObjectPath/index.js';
+
+import $set from '../../atoms/set/index.js';
+
+const merge = (path, value, subject) => Object.keys(value).reduce(
+    (subject, key) => {
+        if (typeof value[key] === 'object') {
+            return merge([...path, key], value[key], subject);
+        }
+
+        return $set([...resolveObjectPath(path), ...key], value[key], subject);
+    },
+    subject
+);
+
+//
+// Deeply merges two objects
+//
+export default createPolymorphFunction(
+    path => value => subject => merge(
+        resolveObjectPath(path),
+        value,
+        subject
+    )
+);
---
+++ src/migrations/molecules/merge/index.spec.js
@@ -0,0 +1,92 @@
+import {expect} from 'chai';
+
+import $merge from './index.js';
+
+describe('Migrations > Atoms > $merge', () => {
+    describe('Common', () => {
+        it('$merge :: String -> Object -> Object -> Object', () => {
+            expect($merge('')).to.be.a('function');
+            expect($merge('')({})).to.be.a('function');
+            expect($merge('')({})({})).not.to.be.a('function');
+        });
+
+        it('$merge :: (String, Object) -> Object -> Object', () => {
+            expect($merge('', {})).to.be.a('function');
+            expect($merge('', {})({})).not.to.be.a('function');
+        });
+
+        it('$merge :: (String, Object, Object) -> Object', () => {
+            expect($merge('', {}, {})).not.to.be.a('function');
+        });
+    });
+
+    describe('Vanilla JS', () => {
+        it('should deeply add properties to an object', () => {
+            const subject = {
+                test: {
+                    a: {
+                        b: {
+                            c: 'c'
+                        }
+                    }
+                }
+            };
+
+            expect($merge('test', {a: {b: {d: 'd'}}}, subject)).to.deep.equal({
+                test: {
+                    a: {
+                        b: {
+                            c: 'c',
+                            d: 'd'
+                        }
+                    }
+                }
+            });
+            expect($merge('test', {a: {b: {d: 'd'}}}, subject)).to.not.equal(subject);
+        });
+
+        it('should deeply overwrite properties in an object without removing any', () => {
+            const subject = {
+                test: {
+                    a: {
+                        b: {
+                            c: 'c'
+                        }
+                    }
+                }
+            };
+
+            expect($merge('test', {a: {b: {c: 'foo'}}}, subject)).to.deep.equal({
+                test: {
+                    a: {
+                        b: {
+                            c: 'foo'
+                        }
+                    }
+                }
+            });
+            expect($merge('test', {a: {b: {c: 'foo'}}}, subject)).to.not.equal(subject);
+        });
+
+        it('should deeply overwrite items of an array without removing any', () => {
+            const subject = {
+                test: {
+                    a: ['a', 'b', 'c']
+                }
+            };
+
+            expect($merge('test', {a: ['foo']}, subject)).to.deep.equal({
+                test: {
+                    a: ['foo', 'b', 'c']
+                }
+            });
+            expect($merge('test', {a: ['foo']}, subject)).to.not.equal(subject);
+        });
+    });
+
+    describe('Immutable', () => {
+        it('should deeply add properties to an object');
+        it('should deeply overwrite properties in an object without removing any');
+        it('should deeply overwrite items of an array without removing any');
+    });
+});
